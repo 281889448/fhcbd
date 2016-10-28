a4a6b303c4129473fc6d29f16f5fe68b497f3e22
Administrator <327778155@qq.com>
2016-10-28 13:38:13 +0800

    通讯录缓存添加

--- Application/Common/Widget/TongxunluWidget.class.php
+++ Application/Common/Widget/TongxunluWidget.class.php
@@ -24,61 +24,76 @@ class TongxunluWidget extends Action {
      */
     public function index(){
         $uid = get_uid();
-        $group = get_group($uid);
         $m = D('User/User');
+        $m->setModel(WEIYUAN);
+        $user = $m->getUser($uid);
+        if(!S('TONGXUNLU_'.$uid)){
 
-        //通讯录数组
-        $group_ids = C('CONTACT_GROUP');
+            $group = get_group($uid);
+            $m = D('User/User');
+
+            //通讯录数组
+            $group_ids = C('CONTACT_GROUP');
 
 
 
-        $group_tmp = array();
-        foreach ($group_ids as $k => $v) {
-            if ($v['is_final'] == true && $v['parent_id'] == 0) {
-                $group_tmp[$k]['name'] = $v['name'];
-            } elseif ($v['is_final'] == true && $v['parent_id'] != 0) {
-                $group_tmp[$v[parent_id]]['name'] = $group_ids[$v[parent_id]]['name'];
-                $group_tmp[$v[parent_id]]['subgroup'][$k][name] = $v['name'];
+            $group_tmp = array();
+            foreach ($group_ids as $k => $v) {
+                if ($v['is_final'] == true && $v['parent_id'] == 0) {
+                    $group_tmp[$k]['name'] = $v['name'];
+                } elseif ($v['is_final'] == true && $v['parent_id'] != 0) {
+                    $group_tmp[$v[parent_id]]['name'] = $group_ids[$v[parent_id]]['name'];
+                    $group_tmp[$v[parent_id]]['subgroup'][$k][name] = $v['name'];
+                }
             }
-        }
-        ksort($group_tmp);
-        $this->assign('contact_group', $group_tmp);
+            ksort($group_tmp);
+            $this->assign('contact_group', $group_tmp);
 
-        if(is_administrator()){
-            $com_name = 'all';
-            $group = 'all';
+            if(is_administrator()){
+                $com_name = 'all';
+                $group = 'all';
+            }else{
+                switch($group){
+                    case '委员':
+                        $m->setModel(WEIYUAN);
+                        $user = $m->getUser($uid);
+                        $com_name = $user['主任'];
+                        break;
+                    case '集体':
+                        $m->setModel(TEAM);
+                        $user = $m->getUser($uid);
+                        $com_name = $user['名称'];
+                        break;
+                }
+
+                //其最上级父类只能为专委会和街道联络委
+                $parent_ids = [1,8];
+                foreach($group_ids as $k=>$v){
+                   if(in_array($v['parent_id'],$parent_ids) && $com_name == $v['name'] ){
+                       $group = $v;
+                       $group['id'] = $k;
+                       break;
+                   }
+
+                }
+
+
+            }
+
+            //以com_name 来看权限
+            $this->assign('group_view',$group);
+
+
+           $content = $this->fetch(T('Application://Common@Widget/tongxunlu'));
+            if($uid == 1 || $user['主任'] !== ''){
+                S('TONGXUNLU_'.$uid,$content,60);
+            }
         }else{
-            switch($group){
-                case '委员':
-                    $m->setModel(WEIYUAN);
-                    $user = $m->getUser($uid);
-                    $com_name = $user['主任'];
-                    break;
-                case '集体':
-                    $m->setModel(TEAM);
-                    $user = $m->getUser($uid);
-                    $com_name = $user['名称'];
-                    break;
+            if($uid == 1 || $user['主任'] !== ''){
+                $content = S('TONGXUNLU_'.$uid);
             }
-
-            //其最上级父类只能为专委会和街道联络委
-            $parent_ids = [1,8];
-            foreach($group_ids as $k=>$v){
-               if(in_array($v['parent_id'],$parent_ids) && $com_name == $v['name'] ){
-                   $group = $v;
-                   $group['id'] = $k;
-                   break;
-               }
-
-            }
-
 
         }
-
-        //以com_name 来看权限
-        $this->assign('group_view',$group);
-
-
-	    $this->display(T('Application://Common@Widget/tongxunlu'));
+        $this->show($content);
     }
 }
